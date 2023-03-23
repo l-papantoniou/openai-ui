2eff1e855f0175aaec0aa881ea658707057737f2
Georgios Marinos <95182300+GiorgosMar@users.noreply.github.com>
2023-03-23 22:09:23 +0200

    add Mui Box, Textfiled, Button

--- src/App.js
+++ src/App.js
@@ -1,31 +1,65 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import axios from 'axios';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 function App() {
-  const [input, setInput] = useState('');
-  const [output, setOutput] = useState('');
+    const [input, setInput] = useState('');
+    const [output, setOutput] = useState('');
 
-  const handleInputChange = event => {
-    setInput(event.target.value);
-  };
+    const handleInputChange = event => {
+        setInput(event.target.value);
+    };
 
-  const handleSubmit = async event => {
-    event.preventDefault();
+    const handleSubmit = async event => {
+        event.preventDefault();
 
-    const response = await axios.post(`http://localhost:5000/chat`);
+        const response = await axios.post(`http://localhost:5000/chat`);
 
-    setOutput(response.data);
-  };
+        setOutput(response.data);
+    };
 
-  return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input type="text" value={input} onChange={handleInputChange} />
-          <button type="submit">Submit</button>
-        </form>
-        {output && <div>{output}</div>}
-      </div>
-  );
+    return (
+        <div>
+            <Box disableGutters
+                 sx={{
+                     ml: 50,
+                     maxWidth: "sm",
+                     marginTop: 7,
+                     display: "flex",
+                     flexDirection: "column",
+                     alignItems: "center",
+                     backgroundColor: "rgba(0,0,0,.5)",
+                     color: "#fff",
+                     borderRadius: "0%",
+                     padding: "20px",
+                 }}>
+                <form onSubmit={handleSubmit}>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            justifyContent: "space-evenly",
+                        }}
+                    >
+                        <TextField id="outlined-basic" label="Outlined" variant="filled" value={input}
+                                   onChange={handleInputChange}/>
+                        <Button
+                            style={{width: "100px"}}
+                            type="submit"
+                            fullWidth
+                            color="secondary"
+                            variant="contained"
+                            sx={{mt: 3, mb: 2}}
+                        >
+                            Submit
+                        </Button>
+                    </Box>
+                </form>
+            </Box>
+            {output && <div>{output}</div>}
+        </div>
+    );
 }
 
 export default App;
